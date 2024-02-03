573b6ce2faf33a0b17fa4528e794800f6ea20055
Doriano <doriano.bergamin18@gmail.com>
2024-02-03 18:27:23 +1100

    array find method

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+function superbowlWin(superbowlArray) {
+  const winningGame = superbowlArray.find(game => game.result === "W");
+
+  // If a winning game is found, return the year, otherwise return undefined
+  return winningGame ? winningGame.year : undefined;
+}
