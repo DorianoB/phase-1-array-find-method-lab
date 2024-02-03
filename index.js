3696bc1c1d12a00c3f0b969cad84f109abc9ed6f
Doriano <doriano.bergamin18@gmail.com>
2024-02-03 18:40:13 +1100

    array find method

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-function superbowlWin(superbowlArray) {
-  const winningGame = superbowlArray.find(game => game.result === "W");
+// written as arrow function
+// function superbowlWin(superbowlArray) {
+//   const winningGame = superbowlArray.find(game => game.result === "W");
 
-  // If a winning game is found, return the year, otherwise return undefined
-  return winningGame ? winningGame.year : undefined;
-}
+//   // If a winning game is found, return the year, otherwise return undefined
+//   return winningGame ? winningGame.year : undefined;
+// }
+
+function isWinningGame(game) {
+    return game.result === "W";
+  }
+  
+  function superbowlWin(superbowlArray) {
+    const winningGame = superbowlArray.find(isWinningGame);
+  
+    // If a winning game is found, return the year, otherwise return undefined
+    return winningGame ? winningGame.year : undefined;
+  }
